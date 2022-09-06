e9dd7772b6705a9b73fc84e6693b43a0d2527bed
TinaChangg <112926091+TinaChangg@users.noreply.github.com>
2022-09-06 19:44:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,35 +30,19 @@ def callback():
 
         return "OK"
 
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+import time
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    get_message = event.message.text
-
-    # Send To Line
-    reply = TextSendMessage(text=f"{get_message}")
-    line_bot_api.reply_message(event.reply_token, reply)
-message = TemplateSendMessage(
-    alt_text='Buttons template',
-    template=ButtonsTemplate(
-        thumbnail_image_url='https://example.com/image.jpg',
-        title='Menu',
-        text='Please select',
-        actions=[
-            PostbackTemplateAction(
-                label='postback',
-                text='postback text',
-                data='action=buy&itemid=1'
-            ),
-            MessageTemplateAction(
-                label='message',
-                text='message text'
-            ),
-            URITemplateAction(
-                label='uri',
-                uri='http://example.com/'
-            )
-        ]
-    )
-)
-line_bot_api.reply_message(event.reply_token, message)
+# 必須放上自己的Channel Access Token
+line_bot_api = LineBotApi('CHANNEL_ACCESS_TOKEN')
+# 請填入您的ID
+yourID = '1657449772'
+# 主動推播訊息
+line_bot_api.push_message(yourID, 
+                          TextSendMessage(text='安安您好！早餐吃了嗎？'))
+# 用迴圈推播訊息
+for i in [1,2,3,4,5]:
+    line_bot_api.push_message(yourID, 
+                              TextSendMessage(text='我們來倒數：'+str(i)))
+    time.sleep(1)
