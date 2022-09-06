94baa2b06c6e27137857f9f6b37fae9cf48fd5f4
TinaChangg <112926091+TinaChangg@users.noreply.github.com>
2022-09-06 19:47:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,19 +30,4 @@ def callback():
 
         return "OK"
 
-from linebot import LineBotApi
-from linebot.models import TextSendMessage
-import time
 
-# 必須放上自己的Channel Access Token
-line_bot_api = LineBotApi('CHANNEL_ACCESS_TOKEN')
-# 請填入您的ID
-yourID = '1657449772'
-# 主動推播訊息
-line_bot_api.push_message(yourID, 
-                          TextSendMessage(text='安安您好！早餐吃了嗎？'))
-# 用迴圈推播訊息
-for i in [1,2,3,4,5]:
-    line_bot_api.push_message(yourID, 
-                              TextSendMessage(text='我們來倒數：'+str(i)))
-    time.sleep(1)
